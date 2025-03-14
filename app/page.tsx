4401c35376b6590704c9115031067e6f8ac3bce0
AJN <buzz781@gmail.com>
2025-03-15 05:01:15 +0900

    feat: 파도배경 및 텍스트 반응형 적용

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@ import WaveBackground from "@/components/WaveBackground";
 export default function Home() {
   return (
     <main className="relative w-full h-screen overflow-hidden bg-white">
-      <WaveBackground />
-      <WaveBackground classname="scale-120 wave two " />
-      <WaveBackground classname="scale-150 wave three animate-pulse" />
+      <WaveBackground classname="" />
+      <WaveBackground classname="scale-180 sm:scale-120 wave two " />
+      <WaveBackground classname="scale-300 sm:scale-150 wave three animate-pulse" />
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white mix-blend-overlay">
-        <div className="text-[20rem] font-bold leading-none">WAVE</div>
-        <div className="text-[20rem] font-bold leading-none">STUDIO</div>
+        <div className="text-[7.5rem] sm:text-[20rem] font-bold leading-none">
+          WAVE
+        </div>
+        <div className="text-[7rem] sm:text-[20rem] font-bold leading-none">
+          STUDIO
+        </div>
       </div>
     </main>
   );
