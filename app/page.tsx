31e999dd78713732a79b34519207229c563bb933
AJN <buzz781@gmail.com>
2025-03-15 15:25:58 +0900

    fix: 색감 변경

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import WaveBackground from "@/components/WaveBackground";
 
 export default function Home() {
   return (
-    <main className="relative w-full h-screen overflow-hidden bg-white">
-      <WaveBackground classname="" />
+    <main className="relative w-full h-screen overflow-hidden bg-white ">
+      <WaveBackground classname="scale-140 sm:scale-100 wave one" />
       <WaveBackground classname="scale-180 sm:scale-120 wave two " />
       <WaveBackground classname="scale-300 sm:scale-150 wave three animate-pulse" />
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white mix-blend-overlay">
         <div className="text-[7.5rem] sm:text-[20rem] font-bold leading-none">
           WAVE
         </div>
-        <div className="text-[7rem] sm:text-[20rem] font-bold leading-none">
+        <WaveBackground classname="scale-300 sm:scale-200 wave three" />
+        <div className="text-[7rem] sm:text-[20rem] font-bold leading-none ">
           STUDIO
         </div>
       </div>
